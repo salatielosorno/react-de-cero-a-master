c5e994a5a2213308c38d0dfde8dcf9d7de4ed7b3
Salatiel Osorno <salatiel.linares@gmail.com>
2020-01-27 15:21:32 -0600

    refactor(App): Move to a folder components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import TarjetaFruta from './componentes/TarjetaFruta';
+import React from "react";
+import TarjetaFruta from "./componentes/TarjetaFruta";
 
 const App = () => (
-	<div>
-		<TarjetaFruta name={'Sandia'} price={5.0}></TarjetaFruta>
-		<TarjetaFruta name={'Naranja'} price={1.5}></TarjetaFruta>
-		<TarjetaFruta name={'Kiwi'} price={3.3}></TarjetaFruta>
-	</div>
+  <div>
+    <TarjetaFruta name={"Sandia"} price={5.0}></TarjetaFruta>
+    <TarjetaFruta name={"Naranja"} price={1.5}></TarjetaFruta>
+    <TarjetaFruta name={"Kiwi"} price={3.3}></TarjetaFruta>
+  </div>
 );
 export default App;
